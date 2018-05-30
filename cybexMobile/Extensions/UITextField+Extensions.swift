1d37afdff56e9fb9002ba6d5c09323e0a3c3ef0b
koofrank <koofranker@gmail.com>
2018-05-30 17:33:43 +0800

    :lipstick: 注册输入时前面图标hightlight

--- cybexMobile/Extensions/UITextField+Extensions.swift
+++ cybexMobile/Extensions/UITextField+Extensions.swift
@@ -88,7 +88,8 @@ class ImageTextField: UITextField {
       imageView.image = image
       // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
       imageView.tintColor = color
-      imageView.alpha = 0.5
+      imageView.alpha = leftView?.alpha ?? 0.5
+      
       leftView = imageView
     } else {
       leftViewMode = UITextFieldViewMode.never
