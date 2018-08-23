4d37161a2696b895c27c7f06d44f8d941e20a172
koofrank <koofranker@gmail.com>
2018-08-23 11:57:01 +0800

    ETO 状态逻辑

---
+++ cybexMobile/Managers/ETOManager.swift
@@ -0,0 +1,142 @@
+//
+//  ETOManager.swift
+//  cybexMobile
+//
+//  Created by koofrank on 2018/8/23.
+//  Copyright © 2018年 Cybex. All rights reserved.
+//
+
+import Foundation
+
+struct ETOStateOption:OptionSet {
+    let rawValue: Int
+    
+    static let unset = ETOStateOption(rawValue: 1 << 0)
+
+    static let login = ETOStateOption(rawValue: 1 << 1)
+    static let notLogin = ETOStateOption(rawValue: 1 << 2)
+   
+    static let KYCPassed = ETOStateOption(rawValue: 1 << 3)
+    static let KYCNotPassed = ETOStateOption(rawValue: 1 << 4)
+
+    static let reserved = ETOStateOption(rawValue: 1 << 5)
+    static let notReserved = ETOStateOption(rawValue: 1 << 6)
+
+    static let bookable = ETOStateOption(rawValue: 1 << 7)
+    static let notBookable = ETOStateOption(rawValue: 1 << 8)
+
+    static let auditPassed = ETOStateOption(rawValue: 1 << 9)
+    static let auditNotPassed = ETOStateOption(rawValue: 1 << 10)
+    static let waitAudit = ETOStateOption(rawValue: 1 << 11)
+
+    static let notStarted = ETOStateOption(rawValue: 1 << 12)
+    static let underway = ETOStateOption(rawValue: 1 << 13)
+    static let finished = ETOStateOption(rawValue: 1 << 14)
+}
+
+enum ETOClauseState {
+    case normal
+    case notShow
+    case checked
+    case checkedAndImmutable
+}
+
+enum ETOJoinButtonStyle {
+    case normal
+    case disable
+}
+
+enum ETOJoinButtonAction {
+    case unset
+    case auditPage
+    case transferPage
+    case icoapePage
+    case unlockPage
+}
+
+enum ETOJoinButtonState {
+    case normal(title: String, style: ETOJoinButtonStyle, action: ETOJoinButtonAction)
+    case disable(title: String, style: ETOJoinButtonStyle)
+    case notShow
+}
+
+class ETOManager {
+    static let shared = ETOManager()
+    
+    private var state = ETOStateOption.unset
+    
+    private init() {
+        
+    }
+    
+    func changeState(_ state:ETOStateOption) {
+        let loginState:ETOStateOption = [.login, .notStarted]
+        let kycState:ETOStateOption = [.KYCPassed, .KYCNotPassed]
+        let userReserveState:ETOStateOption = [.reserved, .notReserved]
+        let projectBookState:ETOStateOption = [.bookable, .notBookable]
+        let auditState:ETOStateOption = [.auditPassed, .auditNotPassed, .waitAudit]
+        let projectState:ETOStateOption = [.notStarted, .underway, .finished]
+        
+        for stateSet in [loginState, kycState, userReserveState, projectBookState, auditState, projectState] {
+            if stateSet.contains(state) {
+                self.state.remove(stateSet)
+                self.state.insert(state.intersection(stateSet))
+            }
+        }
+    }
+    
+    func getClauseState() -> ETOClauseState {
+        if state.contains([.login, .KYCPassed, .notReserved, .bookable]) {
+            return .normal
+        }
+        else if state.contains([.login, .KYCPassed, .reserved, .bookable, .auditPassed, .waitAudit]) {
+            return .checkedAndImmutable
+        }
+        else if state.contains([.notLogin, .bookable, .notStarted, .underway]) {
+            return .checked
+        }
+        
+        return .notShow
+    }
+    
+    func getETOJoinButtonState() -> ETOJoinButtonState {
+        let clause = getClauseState()
+        
+        switch clause {
+        case .normal:
+            return .normal(title: "立即参与", style: .normal, action: .auditPage)
+        case .checkedAndImmutable:
+            if state.contains(.notStarted) || state.contains(.finished) {
+                return .disable(title: "已通过审核", style: .disable)
+            }
+            else if state.contains(.underway) {
+                return .normal(title: "立即参与", style: .normal, action: .transferPage)
+            }
+            else {
+                return .disable(title: "审核中", style: .disable)
+            }
+        case .checked:
+            return .normal(title: "立即参与", style: .normal, action: .unlockPage)
+        case .notShow:
+            if state.contains(.notReserved) {
+                return .disable(title: "立即参与", style: .disable)
+            }
+            else if state.contains([.login, .bookable]) {
+                return .disable(title: "未通过审核", style: .disable)
+            }
+            else if state.contains([.login, .notBookable]) {
+                return .disable(title: "立即参与", style: .disable)
+            }
+            else if state.contains([.login]) {
+                return .normal(title: "进行KYC", style: .normal, action: .icoapePage)
+            }
+            else if state.contains([.notLogin, .finished]) {
+                return .notShow
+            }
+            else {
+                return .disable(title: "停止预约", style: .disable)
+            }
+        }
+      
+    }
+}
