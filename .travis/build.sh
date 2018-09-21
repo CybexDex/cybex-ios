e304b7dda1294a1bcf137766d13500fdfb2f5b43
KooFrank <koofranker@gmail.com>
2018-09-21 10:47:10 +0800

    Update build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash 
 
 if [[ $TRAVIS_BRANCH == 'master' ]];then
+	fastlane publish
+elif [[ $TRAVIS_BRANCH == 'testflight' ]];then
 	fastlane test
 else
-	fastlane test
+	fastlane fir
 fi
