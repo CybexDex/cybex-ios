547b6fa82b8b1b36b0b0d4b63db53f9ad4150692
DKM <393977117@qq.com>
2018-11-27 16:52:38 +0800

    修改XRP的修改功能

--- ChatRoom/ChatService/ChatService.swift
+++ ChatRoom/ChatService/ChatService.swift
@@ -12,9 +12,9 @@ import Repeat
 
 public class ChatService: NSObject {
     // test
-//    static let host = "ws://47.91.242.71:9099/ws"
+    static let host = "ws://47.91.242.71:9099/ws"
     // 正式
-    static let host = "wss://chat.cybex.io/ws"
+//    static let host = "wss://chat.cybex.io/ws"
     lazy var socket = SRWebSocket(url: URL(string: ChatService.host)!)
 
     static var _shared: ChatService?
